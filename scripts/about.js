293bb98e3cd7449a15c2b27c96d346ea99f9aa05
Pestmite <Charleshenrichaki@gmail.com>
2025-05-05 19:24:08 -0400

    Made about page

--- scripts/about.js
+++ scripts/about.js
@@ -1,3 +1,14 @@
 import { selectPage } from "./general.js";
 
 selectPage('About');
+
+
+// Coded in C++ with Arduino
+// Python Backend with server communication
+// Website made with HTML, CSS, and JavaScript
+// Fully 3D-printed protective casing for sprinkler unit
+
+// 150+ hours of development
+// Won Concordia's Award for Outstanding Achievement in Electrical Engineering and Computer Engineering at the Montreal Regional Science Fair
+// Under 20$/unit to make in bulk
+// Received positive feedback and advice
