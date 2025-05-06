4339dd55848c41ac8d4b3abf253ae165a86b0496
Pestmite <Charleshenrichaki@gmail.com>
2025-05-05 21:01:09 -0400

    Made about page interactive

---
+++ data/aspects.js
@@ -0,0 +1,40 @@
+export const aspects = {
+  features: [
+    'Built-in plant database containing data for 50+ plants',
+    'Real-world weather forecast and soil humidity data',
+    '15+ user tunable variables',
+    'Machine-learning inpired algorithm for weather analysis'
+  ], stack: [
+    'Coded in C++ with Arduino',
+    'Python Backend with server communication',
+    'Website made with HTML, CSS, and JavaScript',
+    'Fully 3D-printed protective casing for sprinkler unit'
+  ], impact: [
+    '150+ hours of development',
+    'Won Concordia Award for Outstanding Achievement in ECE',
+    'Under 20$/unit to make in bulk',
+    'Received positive feedback and advice',
+  ]
+}
+
+export function switchSection(tab) {
+  document.querySelectorAll('.about-select-js').forEach((link) => {
+    link.classList.remove('selected-section')
+  });
+
+  tab.classList.add('selected-section');
+  generateAspects(tab.id);
+}
+
+export function generateAspects(section) {
+  let HTML = '';
+
+  aspects[section].forEach((element) => {
+    HTML += `<div class="features-group">
+      <img src="images/check-list.png">
+      <p>${element}</p>
+    </div>`
+  });
+
+  document.querySelector('.aspect-section-js').innerHTML = HTML;
+}
--- scripts/about.js
+++ scripts/about.js
@@ -1,26 +1,9 @@
 import { selectPage } from "./general.js";
+import { generateAspects, switchSection } from "../data/aspects.js";
 
 document.querySelectorAll('.about-select-js').forEach((tab) => {
   tab.addEventListener('click', () => switchSection(tab))
 });
 
-function switchSection(tab) {
-  document.querySelectorAll('.about-select-js').forEach((link) => {
-    link.classList.remove('selected-section')
-  });
-
-  tab.classList.add('selected-section');
-}
-
 selectPage('About');
-
-
-// Coded in C++ with Arduino
-// Python Backend with server communication
-// Website made with HTML, CSS, and JavaScript
-// Fully 3D-printed protective casing for sprinkler unit
-
-// 150+ hours of development
-// Won Concordia's Award for Outstanding Achievement in Electrical Engineering and Computer Engineering at the Montreal Regional Science Fair
-// Under 20$/unit to make in bulk
-// Received positive feedback and advice
+generateAspects('features');
